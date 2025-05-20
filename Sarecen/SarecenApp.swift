081f2bf48a48dec64420869c01f3d43381d1053a
Radcabna <radcabna@gmail.com>
2025-05-20 13:49:27 +0300

    Initial Commit

---
+++ Sarecen/SarecenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SarecenApp.swift
+//  Sarecen
+//
+//  Created by Алкександр Степанов on 20.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SarecenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
